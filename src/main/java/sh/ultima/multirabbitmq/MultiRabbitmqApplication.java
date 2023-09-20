aef8c928a8f4ce7d59b36827bab9d65585b6f33a
Alex Lindemann <exmann94@gmail.com>
2023-09-19 21:11:55 -0500

    init

---
+++ src/main/java/sh/ultima/multirabbitmq/MultiRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package sh.ultima.multirabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/ultima/multirabbitmq/MultiRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.ultima.multirabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
